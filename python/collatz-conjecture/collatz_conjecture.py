22f21a525d446a4e421ecd1a98bac485651f8c93
Pranjal Gupta <pranjalg131@gmail.com>
2025-01-08 15:15:05 +0530

    feat: solutions for conditionals and boolean sections in python

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,17 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    
+    ans = 0;
+    # while(number != 1):
+    #     if(number % 2 == 0):
+    #         number = number / 2
+    #     else:
+    #         number = number * 3 + 1
+    #     ans += 1
+    
+    while(number != 1):
+        number = number / 2 if number % 2 == 0 else number * 3 + 1
+        ans += 1
+    
+    return ans
